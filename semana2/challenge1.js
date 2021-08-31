7dd91f8d8aa738b28c2acdef27506c190dc0726d
Juan Ceballos <jacu29@gmail.com>
2021-08-31 18:42:00 -0500

    finish the challenge 1

--- semana2/challenge1.js
+++ semana2/challenge1.js
@@ -1,5 +1,19 @@
 function twoSums(numbs, target) {
     //Implementación
+    let solution = []
+    for (let i = 0; i < numbs.length; i++) {
+        const a = numbs[i];
+        for (let j = 0; j < numbs.length; j++) {
+            const b = numbs[j];
+            if(a + b === target && i !== j && solution.length<2){
+                solution.push(i)
+                solution.push(j)
+            }
+        }
+    }
+    return solution
 }
+
+twoSums([3,3], 6)
 
 module.exports = twoSums;
