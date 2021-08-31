068af85a37550544cc86896412d075a9a99e4803
Juan Ceballos <jacu29@gmail.com>
2021-08-30 19:25:52 -0500

    ---

--- semana1/challenge1.js
+++ semana1/challenge1.js
@@ -1,27 +1,28 @@
-const reverseString = (str) => {
-    //Implementación
-    const type = typeof str;
-    let length = str.length
-    let s = str;
-    let reverse = '';
 
-    if(type !== "string"){
-        throw new Error("Error: Tipo de dato o longitud no admitidos.");
-    }
-    if(length <= 1){
+const reverseString = (str) => {
+  //Implementación
+
+  let length = str.length
+  let s = str;
+  let reverse = '';
+
+  if(typeof str !== "string"){
       throw new Error("Error: Tipo de dato o longitud no admitidos.");
-    }
-    if(length > 15){
-      throw new Error("Error: Tipo de dato o longitud no admitidos.");
-    }
-    let i = s.length-1;
-    while(i >= 0){
-      reverse += s[i];
-      i--;
-    }
-    return reverse;
+  }
+  if(length <= 1){
+    throw new Error("Error: Tipo de dato o longitud no admitidos.");
+  }
+  if(length > 15){
+    throw new Error("Error: Tipo de dato o longitud no admitidos.");
+  }
+  let i = s.length-1;
+  while(i >= 0){
+    reverse += s[i];
+    i--;
+  }
+  return reverse;
 }
 
 module.exports = {
-    reverseString
+  reverseString
 }
