2af902f16affb13a6c94b02d5ecc3b0d5dabe3ad
Juan Ceballos <jacu29@gmail.com>
2021-09-01 20:27:57 -0500

    Finish challenge 4

--- semana2/challenge4.js
+++ semana2/challenge4.js
@@ -1,6 +1,9 @@
-function landMass() {
+function landMass(country, landMass) {
     //Implementación
-    return 0;
+    const TOTAL_LAND_MASS = 148940000
+    const mass = (landMass*100)/TOTAL_LAND_MASS
+    const percent = parseFloat(mass.toFixed(2))
+    return { "percent":percent, "message":`${country} repenseta el ${percent}% de la masa de la tierra`}
 }
 
 module.exports = landMass;
