89d10785ccbe2e66191f183d40a6e4927f81c973
Juan Ceballos <jacu29@gmail.com>
2021-09-06 22:19:52 -0500

    Solucionar challenge 1

--- semana3/challenge1.js
+++ semana3/challenge1.js
@@ -1,5 +1,19 @@
 function countPrimes(number) {
     //Implementación
+    if(number > 0){
+        let primes = []
+        for (let i = 2; i < number; i++) {
+            if(Math.floor(Math.sqrt(i))=== 1) primes.push(i)
+            else {
+                let divisors = 0;
+                for (let j = 0; j < primes.length; j++) {
+                    if(i % primes[j] === 0) divisors++                   
+                }
+                if (divisors === 0) primes.push(i)
+            }
+        }
+        return primes.length
+    }
     return 0;
 }
 
