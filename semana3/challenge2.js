6a9147200605966e5d6d3cf22091842e1eb5ce9c
Juan Ceballos <jacu29@gmail.com>
2021-09-21 17:15:19 -0500

    Finalizar Challenger 2

--- semana3/challenge2.js
+++ semana3/challenge2.js
@@ -1,6 +1,7 @@
 function minMax(aNumbs) {
     //Implementación
-    return 0;
+    aNumbs.sort((a, b) => {return a - b})
+    return {min:aNumbs[0], max:aNumbs[aNumbs.length - 1]}
 }
 
 module.exports = minMax;
