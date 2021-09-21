fe7981d84d2c6d9573f0d2cef1e6bbec7d844d3c
Juan Ceballos <jacu29@gmail.com>
2021-09-21 18:49:31 -0500

    Finalizado challenge 3

--- semana3/challenge3.js
+++ semana3/challenge3.js
@@ -1,5 +1,16 @@
-function mcm (a, b) {
-  return 0;
+function mcd(a,b){
+  let c;
+  while(b){
+    c = b;
+    b = a % b;
+    a = c
+  }
+  return a;
 }
+
+function mcm (a, b) {
+  return (a * b)/mcd(a,b)
+}
+
 
 module.exports = mcm;
