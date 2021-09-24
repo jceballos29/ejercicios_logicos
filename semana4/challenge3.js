ffdd4c58b609f28eee24d9622862bd8dc27df60e
Juan Ceballos <jacu29@gmail.com>
2021-09-23 22:57:06 -0500

    Challenge 3 completado

--- semana4/challenge3.js
+++ semana4/challenge3.js
@@ -1,5 +1,20 @@
 function isSpecialArray (specialArr) {
-  return 0;
+
+  const even = specialArr.filter((element, index) => {
+    if(index%2===0){
+      return element;
+    }
+  })
+  const odd = specialArr.filter((element, index) => {
+    if(index%2!==0){
+      return element;
+    }
+  })
+
+  return even.every(element => element%2===0) && odd.every(element => element%2===1);
 }
+
+const result = isSpecialArray([2, 7, 8, 8, 6, 1, 6, 3])
+console.log(result);
 
 module.exports = isSpecialArray;
