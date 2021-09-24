78816fd871afd4a5c6fc47fe870b639d3d661cf0
Juan Ceballos <jacu29@gmail.com>
2021-09-23 22:28:19 -0500

    Challenge 2 Completado

--- semana4/challenge2.js
+++ semana4/challenge2.js
@@ -1,6 +1,11 @@
 function isEqual(objInput1, objInput2) {
     //Implementación
-    return 0;
+    const input1 = Object.entries(objInput1).join();
+    const input2 = Object.entries(objInput2).join()
+
+    return input1 === input2;
 }
+const result = isEqual({a: 1, b: 2, c: 0}, {a: 1, b: 2})
+console.log(result);
 
 module.exports = isEqual;
